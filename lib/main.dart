66336ad8555514d1534eb951de788441e566d98f
naimkatiman <naimkatiman@gmail.com>
2024-08-11 23:39:07 +0800

    refactor: Update IntroductionAnimationScreen to include animation controller and views

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,117 @@
-import 'dart:io';
-import 'package:best_flutter_ui_templates/app_theme.dart';
+import 'package:best_flutter_ui_templates/introduction_animation/components/care_view.dart';
+import 'package:best_flutter_ui_templates/introduction_animation/components/center_next_button.dart';
+import 'package:best_flutter_ui_templates/introduction_animation/components/mood_diary_vew.dart';
+import 'package:best_flutter_ui_templates/introduction_animation/components/relax_view.dart';
+import 'package:best_flutter_ui_templates/introduction_animation/components/splash_view.dart';
+import 'package:best_flutter_ui_templates/introduction_animation/components/top_back_skip_view.dart';
+import 'package:best_flutter_ui_templates/introduction_animation/components/welcome_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'navigation_home_screen.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await SystemChrome.setPreferredOrientations(<DeviceOrientation>[
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown
-  ]).then((_) => runApp(MyApp()));
+class IntroductionAnimationScreen extends StatefulWidget {
+  const IntroductionAnimationScreen({Key? key}) : super(key: key);
+
+  @override
+  _IntroductionAnimationScreenState createState() =>
+      _IntroductionAnimationScreenState();
 }
 
-class MyApp extends StatelessWidget {
+class _IntroductionAnimationScreenState
+    extends State<IntroductionAnimationScreen> with TickerProviderStateMixin {
+  AnimationController? _animationController;
+
+  @override
+  void initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 8));
+    _animationController?.animateTo(0.0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.dark,
-      statusBarBrightness:
-          !kIsWeb && Platform.isAndroid ? Brightness.dark : Brightness.light,
-      systemNavigationBarColor: Colors.white,
-      systemNavigationBarDividerColor: Colors.transparent,
-      systemNavigationBarIconBrightness: Brightness.dark,
-    ));
-    return MaterialApp(
-      title: 'Flutter UI',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        textTheme: AppTheme.textTheme,
-        platform: TargetPlatform.iOS,
+    print(_animationController?.value);
+    return Scaffold(
+      backgroundColor: Color(0xffF7EBE1),
+      body: ClipRect(
+        child: Stack(
+          children: [
+            SplashView(
+              animationController: _animationController!,
+            ),
+            RelaxView(
+              animationController: _animationController!,
+            ),
+            CareView(
+              animationController: _animationController!,
+            ),
+            MoodDiaryVew(
+              animationController: _animationController!,
+            ),
+            WelcomeView(
+              animationController: _animationController!,
+            ),
+            TopBackSkipView(
+              onBackClick: _onBackClick,
+              onSkipClick: _onSkipClick,
+              animationController: _animationController!,
+            ),
+            CenterNextButton(
+              animationController: _animationController!,
+              onNextClick: _onNextClick,
+            ),
+          ],
+        ),
       ),
-      home: NavigationHomeScreen(),
     );
   }
-}
 
-class HexColor extends Color {
-  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+  void _onSkipClick() {
+    _animationController?.animateTo(0.8,
+        duration: Duration(milliseconds: 1200));
+  }
 
-  static int _getColorFromHex(String hexColor) {
-    hexColor = hexColor.toUpperCase().replaceAll('#', '');
-    if (hexColor.length == 6) {
-      hexColor = 'FF' + hexColor;
+  void _onBackClick() {
+    if (_animationController!.value >= 0 &&
+        _animationController!.value <= 0.2) {
+      _animationController?.animateTo(0.0);
+    } else if (_animationController!.value > 0.2 &&
+        _animationController!.value <= 0.4) {
+      _animationController?.animateTo(0.2);
+    } else if (_animationController!.value > 0.4 &&
+        _animationController!.value <= 0.6) {
+      _animationController?.animateTo(0.4);
+    } else if (_animationController!.value > 0.6 &&
+        _animationController!.value <= 0.8) {
+      _animationController?.animateTo(0.6);
+    } else if (_animationController!.value > 0.8 &&
+        _animationController!.value <= 1.0) {
+      _animationController?.animateTo(0.8);
     }
-    return int.parse(hexColor, radix: 16);
+  }
+
+  void _onNextClick() {
+    if (_animationController!.value >= 0 &&
+        _animationController!.value <= 0.2) {
+      _animationController?.animateTo(0.4);
+    } else if (_animationController!.value > 0.2 &&
+        _animationController!.value <= 0.4) {
+      _animationController?.animateTo(0.6);
+    } else if (_animationController!.value > 0.4 &&
+        _animationController!.value <= 0.6) {
+      _animationController?.animateTo(0.8);
+    } else if (_animationController!.value > 0.6 &&
+        _animationController!.value <= 0.8) {
+      _signUpClick();
+    }
+  }
+
+  void _signUpClick() {
+    Navigator.pop(context);
   }
 }
