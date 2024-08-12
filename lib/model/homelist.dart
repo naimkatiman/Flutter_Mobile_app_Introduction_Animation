4178b9e4f7b285edfeaefd7c55f165a018dda83d
naimkatiman <naimkatiman@gmail.com>
2024-08-12 12:59:56 +0800

    refactor: Update import statements for introduction animation components

--- lib/model/homelist.dart
+++ lib/model/homelist.dart
@@ -1,6 +1,4 @@
-import 'package:best_flutter_ui_templates/design_course/home_design_course.dart';
-import 'package:best_flutter_ui_templates/fitness_app/fitness_app_home_screen.dart';
-import 'package:best_flutter_ui_templates/hotel_booking/hotel_home_screen.dart';
+
 import 'package:best_flutter_ui_templates/introduction_animation/introduction_animation_screen.dart';
 import 'package:flutter/widgets.dart';
 
@@ -18,17 +16,6 @@ class HomeList {
       imagePath: 'assets/introduction_animation/introduction_animation.png',
       navigateScreen: IntroductionAnimationScreen(),
     ),
-    HomeList(
-      imagePath: 'assets/hotel/hotel_booking.png',
-      navigateScreen: HotelHomeScreen(),
-    ),
-    HomeList(
-      imagePath: 'assets/fitness_app/fitness_app.png',
-      navigateScreen: FitnessAppHomeScreen(),
-    ),
-    HomeList(
-      imagePath: 'assets/design_course/design_course.png',
-      navigateScreen: DesignCourseHomeScreen(),
-    ),
+
   ];
 }
